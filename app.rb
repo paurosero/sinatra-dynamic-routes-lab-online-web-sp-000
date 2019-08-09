c95b4bc264212ef4f4f8616ee64eb2e19506ab9c
Paul Rosero <paul_rosero@yahoo.com>
2019-08-09 16:56:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -24,7 +24,21 @@ class App < Sinatra::Base
   end
   
   get '/:operation/:number1/:number2' do
-    @operation = params[:operation]
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
+
+    answer = ''
+
+    case params[:operation]
+    when 'add'
+      answer = (number1 + number2).to_s
+    when 'subtract'
+      answer = (number1 - number2).to_s
+    when 'multiply'
+      answer = (number1 * number2).to_s
+    when 'divide'
+      answer = (number1 / number2).to_s
+    end
   end
 
 end
